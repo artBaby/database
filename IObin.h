b75aa4bc693259fa5339da7ea7479b4f1691fb9f
artBaby <artemiy5555563@gmail.com>
2015-01-29 22:56:07 +0300

    Update IObin.h

--- IObin.h
+++ IObin.h
@@ -5,40 +5,19 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <time.h>
+#include <string.h>
 #include <windows.h>
-//#include "main.h"
-//typedef BMP_HEADER *HEADER;
+//#include "treap.h"
+#include "main.h"
 
-#pragma pack(push, 1)
+//int buferr;
+//int realwidth(int width);
+//FILE* OpenF(char *filename);
 
-typedef struct
-{
-	char signature[2];
-	unsigned int fileSize;
-	unsigned short int reserved1;
-	unsigned short int reserved2;
-	unsigned int offset;
-	unsigned int headerLength;
-	unsigned int width;
-	unsigned int height;
-	short numLayers;
-	short bpp;
-	unsigned int compression;
-	unsigned int bitmapLength;
-	unsigned int horResolution;
-	unsigned int verResolution;
-	unsigned int numColors;
-	unsigned int numMainColors;
-} BMP_HEADER;
+//void LoadBMPData(unsigned char *image, unsigned int size, FILE *filename);
+void WriteBin(char *filename, char* data, long int data_length);
+char *ReadBin(char *value, char *buf);
+size_t GetSize(char *value);
 
-#pragma pack(pop)
-
-int buferr;
-int realwidth(int width);
-FILE* OpenF(char *filename);
-BMP_HEADER *GetHeader(FILE *filename);
-void LoadBMPData(unsigned char *image, unsigned int size, FILE *filename);
-int SaveBMPData(char *filename, BMP_HEADER *header, unsigned char *image);
-unsigned char *ReadBMP(FILE *file, BMP_HEADER *header);
 
 #endif // !IOBIN_H
