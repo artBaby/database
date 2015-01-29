8eb7392c598cc8acf00b4f3bf4a334b28cd96d03
artBaby <artemiy5555563@gmail.com>
2015-01-29 22:56:36 +0300

    Update memget.h

--- memget.h
+++ memget.h
@@ -2,6 +2,6 @@
 #define MEMGET_H
 #include "main.h"
 
-kvp_t *memget(kvp_t *database, char* key, char* value, data_type_t data_type_t);
+kvp_t *memget(kvp_t *database, char* key, char* value, data_type_t data_type_t, size_t data_length);
 
 #endif // !MEMGET_H
